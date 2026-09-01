16997574a8c0834b5d189d0892f5e531b0e4eb0e
Nancy hesham <nancyhesham@Nancys-MacBook-Air.local>
2026-09-02 00:07:40 +0300

    Restore session-2 project and questions

--- Sessions/session-2/problem-2/main.js
+++ Sessions/session-2/problem-2/main.js
@@ -1,3 +1,14 @@
+// Question 1: Check if Array is Sorted
+function isSortedAscending(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Question 2: Return Numbers Greater Than a Value
 function numbersGreaterThan(arr, value) {
   const result = [];
 
@@ -10,5 +21,10 @@ function numbersGreaterThan(arr, value) {
   return result;
 }
 
+console.log("Sorted check:");
+console.log(isSortedAscending([1, 2, 3, 4, 5]));
+console.log(isSortedAscending([1, 3, 2, 4]));
+
+console.log("Numbers greater than value:");
 console.log(numbersGreaterThan([10, 5, 20, 3, 15], 8));
 console.log(numbersGreaterThan([2, 4, 6], 10));
